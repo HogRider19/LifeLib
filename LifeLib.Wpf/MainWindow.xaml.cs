38bfb624fad3d1391b93d6fe08625cb9176fa2cb
Дмитренко Данил <danilzzzg19@gmail.com>
2025-08-24 21:32:34 +0300

    Init!

---
+++ LifeLib.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Wpf;
+
+namespace LifeLib.Wpf;
+
+public partial class MainWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        
+        OpenTkControl.Start(new GLWpfControlSettings
+        {
+            MajorVersion = 3,
+            MinorVersion = 3
+        });
+    }
+
+    private void OpenTkControl_OnRender(TimeSpan obj)
+    {
+        GL.ClearColor(Color4.Blue);
+        GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+    }
+}
